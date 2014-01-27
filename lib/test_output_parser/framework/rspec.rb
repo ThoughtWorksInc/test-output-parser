9a716dd2b6ab3d2527116e0ffaffe46792380c68
Akshay Karle <akshay.a.karle@gmail.com>
2014-01-27 18:39:38 -0300

    Return a hash of test summary instead of an array

--- lib/test_output_parser/framework/rspec.rb
+++ lib/test_output_parser/framework/rspec.rb
@@ -2,14 +2,14 @@ module TestOutputParser
   module Framework
     class RSpec
       def self.count(test_output)
-        total_specs, failed_specs, pending_specs = 0, 0, 0
+        summary = Hash.new(0)
         test_output.scan(/(\d+)\s+example[s]?,\s+(\d+)\s+failure[s]?(,\s*(\d+)\s+pending)?/).each do |arr|
-          total_specs  += arr[0].to_i
-          failed_specs += arr[1].to_i
-          pending_specs += arr[3].to_i
+          summary[:total]   += arr[0].to_i
+          summary[:failed]  += arr[1].to_i
+          summary[:pending] += arr[3].to_i
         end
 
-        [total_specs, failed_specs, pending_specs]
+        summary
       end
     end
   end
--- spec/test_output_parser_spec.rb
+++ spec/test_output_parser_spec.rb
@@ -2,26 +2,26 @@ require 'spec_helper'
 
 describe TestOutputParser do
   it 'should count the number of specs ran for one example' do
-    TestOutputParser.count("1 example, 0 failures").should == [1, 0, 0]
+    TestOutputParser.count("1 example, 0 failures").should == {:total => 1, :failed => 0, :pending => 0}
   end
 
   it 'should count the number of specs ran for one rspec run' do
-    TestOutputParser.count("25 examples, 0 failures").should == [25, 0, 0]
+    TestOutputParser.count("25 examples, 0 failures").should == {:total => 25, :failed => 0, :pending => 0}
   end
 
   it 'should count the number of specs ran for multiple rspec runs' do
-    TestOutputParser.count("25 examples, 1 failure\n\n\n5 examples, 3 failures").should == [30, 4, 0]
+    TestOutputParser.count("25 examples, 1 failure\n\n\n5 examples, 3 failures").should == {:total => 30, :failed => 4, :pending => 0}
   end
 
   it 'should ignore lines not related to rspec test summary' do
-    TestOutputParser.count(File.read("spec/fixtures/sample-rspec-output.txt")).should == [67, 0, 0]
+    TestOutputParser.count(File.read("spec/fixtures/sample-rspec-output.txt")).should == {:total => 67, :failed => 0, :pending => 0}
   end
 
   it 'should count failures correctly' do
-    TestOutputParser.count(File.read("spec/fixtures/sample-failed-rspec-output.txt")).should == [1460, 1, 3]
+    TestOutputParser.count(File.read("spec/fixtures/sample-failed-rspec-output.txt")).should == {:total => 1460, :failed => 1, :pending => 3}
   end
 
   it 'should count the number of pending specs' do
-    TestOutputParser.count(File.read("spec/fixtures/sample-failed-rspec-output.txt")).should == [1460, 1, 3]
+    TestOutputParser.count(File.read("spec/fixtures/sample-failed-rspec-output.txt")).should == {:total => 1460, :failed => 1, :pending => 3}
   end
 end
